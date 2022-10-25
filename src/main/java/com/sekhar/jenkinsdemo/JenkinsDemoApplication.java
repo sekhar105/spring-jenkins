db7907445c37cdbad0df0c5fe6e4ef17ad82f698
sekhar105 <repaka99999>
2022-10-25 09:52:43 +0530

    first commit

---
+++ src/main/java/com/sekhar/jenkinsdemo/JenkinsDemoApplication.java
@@ -0,0 +1,26 @@
+package com.sekhar.jenkinsdemo;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDemoApplication {
+
+public static	Logger logger= LoggerFactory.getLogger(JenkinsDemoApplication.class);
+	
+	@PostConstruct
+	public void postConstruct() {
+		logger.info("application started....");
+	}
+	
+	public static void main(String[] args) {
+		
+		logger.info("applicatoin executed");
+		SpringApplication.run(JenkinsDemoApplication.class, args);
+	}
+
+}
